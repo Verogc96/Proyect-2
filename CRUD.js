b773fadce7ba3cf319ae3ecac99ab0801db2cd4f
Vero96 <vgutierrezca@est.utn.ac.cr>
2022-12-14 00:08:00 -0600

    Log in, registro, settings, dashboard, general view work properly

--- CRUD.js
+++ CRUD.js
@@ -3,14 +3,14 @@ var loggedUser = "";
 var actualUsername = localStorage;
 var actualIdUser = localStorage;
 
-var users = [
+/*var users = [
     { "ID": 1, "firstName": "Veronica", "lastName": "Gutierrez", "phone": "8557-7823", "username": "vgutierrez", "password": "123" },
     { "ID": 2, "firstName": "Bladimir", "lastName": "Arroyo", "phone": "8875-1861", "username": "barroyo", "password": "123" }
-];
+];*/
 
 
 
-/*let userSettings = [
+/*var userSettings = [
     { "userID": '1', fullName: 'Veronica Gutierrez', speedAverage: '60 km/h', aboutMe: 'Something about me goes here' },
     { "userID": '2', fullName: 'Bladimir Arroyo', speedAverage: '80 km/h', aboutMe: 'Something about me goes here' }
 ];*/
@@ -19,25 +19,25 @@ let rides = [
     {
         "ID": 1, "rideName": "Brete", "startFrom": "Barrio Los Angeles, Ciudad Quesada", "end": "Mercado Municipal, Ciudad Quesada",
         "description": "This is my everyday ride, from Barrio Los Angeles to my job office in Second Floor of Coopeservidores Building",
-        "departure": "6:45 AM", "estimatedArrival": "7:05 AM", "userID": '1'
+        "departure": "6:45 AM", "estimatedArrival": "7:05 AM", "username": 'vgutierrez'
     },
 
     {
         "ID": 2, "rideName": 'Aeropuerto', "startFrom": 'Barrio San Antonio, Ciudad Quesada', "end": 'AIJS, Alajuela',
         "description": 'This a ride from Ciudad Quesada to Juan Santamaria International Airport',
-        "departure": '8:00 AM', "estimatedArrival": '10:00 AM', "userID": '1'
+        "departure": '8:00 AM', "estimatedArrival": '10:00 AM', "username": 'vgutierrez'
     },
 
     {
         "ID": 3, "rideName": 'Aeropuerto', "startFrom": 'Terminal de Buses, Ciudad Quesada', "end": 'AIJS, Alajuela',
         "description": 'This a ride from Ciudad Quesada to Juan Santamaria International Airport',
-        "departure": '12:00 MD', "estimatedArrival": '02:00 PM', "userID": '2'
+        "departure": '12:00 MD', "estimatedArrival": '02:00 PM', "username": 'barroyo'
     },
 
     {
         "ID": 4, "rideName": 'Brete', "startFrom": 'Barrio Los Angeles, Ciudad Quesada', "end": 'Parque Municipal, Ciudad Quesada',
         "description": 'This is my everyday ride, from Barrio Los Angeles to my job office in Municipalidad San Carlos',
-        "departure": '06:30 AM', "estimatedArrival": '06:45 am', "userID": '2'
+        "departure": '06:30 AM', "estimatedArrival": '06:45 am', "username": 'barroyo'
     }
 ];
 
@@ -46,11 +46,23 @@ let rides = [
 function loadEvents() {
     // bind the click event to login button
     jQuery('#login-button').bind('click', function (element) {
+        element.preventDefault();
         validateLogIn();
     });
 
     jQuery('#register-button').bind('click', function (element) {
+        element.preventDefault();
         registerUser();
+    });
+
+    jQuery('#searchButton').bind('click', function (element) {
+        element.preventDefault();
+        createGeneralTable();
+    });
+
+    jQuery('#btnSave').bind('click', function (element) {
+        element.preventDefault();
+        modifyUserSettings();
     });
     /*
             // bind the click event to all edit buttons
@@ -122,6 +134,15 @@ function loadUser() {
     var actualUser = document.querySelector("#user");
     actualUser.innerHTML = localStorage.getItem("username");
 }
+/*
+function tableStyles() {
+    var table = document.getElementsByClassName("ridesTable");
+
+    table.setAttribute("class", "table");
+    table.setAttribute("class", "table,th,td");
+    table.setAttribute("class", "table tbody tr:nth-child(odd)");
+    table.setAttribute("class", "table tbody tr:nth-child(even)");
+}*/
 
 function createTableRidesperUser() {
     $(".tblMyRides").append('<tr><th>Name</th>' +
@@ -129,7 +150,7 @@ function createTableRidesperUser() {
         '<th>End</th>' +
         '<th>Actions</th><tr>');
     for (var i = 0; i < rides.length; i++) {
-        if (rides[i].userID == localStorage.getItem("ID")) {
+        if (rides[i].username == localStorage.getItem("username")) {
             $(".tblMyRides").append(
                 '<tr>' +
                 '<td>' + rides[i].rideName + '</td>' +
@@ -141,6 +162,66 @@ function createTableRidesperUser() {
         }
     }
 }
+/*
+function deleteFunction() {
+    var rowCount = document.getElementById("tblAllRides").length;
+    var table = document.getElementById("tblAllRides");
+
+    for (var i = rowCount - 1; i > 0; i--) {
+        table.deleteRow(i);
+    }
+}*/
+
+function createGeneralTable() {
+    var start = document.getElementById('from').value;
+    var end = document.getElementById('to').value;
+    var rowCount = document.getElementById("tblAllRides").length;
+    var table = document.getElementById("tblAllRides");
+    var tableHeader = $(".ridesTable").append('<tr><th>User</th>' +
+        '<th>Start</th>' +
+        '<th>End</th>' +
+        '<th></th><tr>');
+
+    if (rowCount > 1) {
+        for (var i = 0; i >= rowCount; i++) {
+            table.deleteRow(i);
+        }
+        tableHeader;
+        for (var i = 0; i < rides.length; i++) {
+            if (rides[i].startFrom == start) {
+                if (rides[i].end == end) {
+                    $(".ridesTable").append(
+                        '<tr>' +
+                        '<td>' + rides[i].username + '</td>' +
+                        '<td>' + rides[i].startFrom + '</td>' +
+                        '<td>' + rides[i].end + '</td>' +
+                        '<td><a href="ViewRide.html">View</a></td>' +
+                        '</tr>'
+                    );
+                }
+            }
+        }
+    } else {
+        tableHeader;
+        for (var i = 0; i < rides.length; i++) {
+            if (rides[i].startFrom == start) {
+                if (rides[i].end == end) {
+                    $(".ridesTable").append(
+                        '<tr>' +
+                        '<td>' + rides[i].username + '</td>' +
+                        '<td>' + rides[i].startFrom + '</td>' +
+                        '<td>' + rides[i].end + '</td>' +
+                        '<td><a href="ViewRide.html">View</a></td>' +
+                        '</tr>'
+                    );
+                }
+            }
+        }
+
+    }
+
+}
+
 
 function logOut() {
     localStorage.removeItem('username');
@@ -162,44 +243,48 @@ function registerUser() {
     var phone = fPhone.value;
     var username = fUsername.value;
     var password = fPassword.value;
-    
-    var user = { "ID": users.length + 1, "firstName": firstName, "lastName": lastName, "phone": phone, "username": username, "password": password };
-    users.push(user);
-    
-    window.localStorage.setItem('users', JSON.stringify(users));
-    
+
+    var usersCopy = JSON.parse(localStorage.getItem("users"));
+
+    var user = { "ID": usersCopy.length + 1, "firstName": firstName, "lastName": lastName, "phone": phone, "username": username, "password": password };
+    usersCopy.push(user);
+
+    window.localStorage.setItem('users', JSON.stringify(usersCopy));
+
+
     window.alert('Register Successfully. Welcome to Tico Rides');
     window.location.href = "RegisterUser.html";
 }
 
-/*
- function create(data) {
-    this.#data.push(data);
-    return this.#data.lenght;
+function fillOutSettingsForm() {
+
+    var dbSettings = JSON.parse(localStorage.getItem('userSettings'));
+    var resultado = dbSettings.find(e => e.userID == JSON.parse(localStorage.getItem('ID')));
+    document.getElementById('fullname').value= resultado.fullName;
+    document.getElementById('speedaverage').value= resultado.speedAverage;
+    document.getElementById('aboutme').value= resultado.aboutMe;
 }
 
-function read(id) {
-    return this.#data[id];
+
+function modifyUserSettings() {
+    var dbUserSettings = JSON.parse(localStorage.getItem("userSettings"));
+
+    var inFullName = document.getElementById('fullname').value;
+    var inSpeedAverage = document.getElementById('speedaverage').value;
+    var inAboutMe = document.getElementById('aboutme').value;
+
+    var objIndex = dbUserSettings.findIndex((obj => obj.userID == JSON.parse(localStorage.getItem('ID'))));
+
+    dbUserSettings[objIndex].fullName = inFullName;
+    dbUserSettings[objIndex].speedAverage = inSpeedAverage;
+    dbUserSettings[objIndex].aboutMe = inAboutMe;
+
+    localStorage.setItem('userSettings', JSON.stringify(dbUserSettings));
+    window.alert("Settings updated correctly");
+    window.location.href = "Dashboard.html";
 }
 
-function readAll() {
-    return this.#data;
-}
-
-function update(id, data) {
-    this.#data[id] = data;
-    return true;
-}
-
-function deleteRide (id) {
-    this.#data.splice(id, 1);
-    return true;
-}
-
-*/
-
-
-loadEvents();
 loadUser();
 createTableRidesperUser();
-
+fillOutSettingsForm();
+loadEvents();
